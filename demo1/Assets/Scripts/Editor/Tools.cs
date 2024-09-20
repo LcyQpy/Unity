db64ca7e24b831a0202a4ea866f7c8bcde9a42f5
kunavya <2136570886@qq.com>
2024-09-20 18:42:47 +0800

    FindPeference

--- demo1/Assets/Scripts/Editor/Tools.cs
+++ demo1/Assets/Scripts/Editor/Tools.cs
@@ -4,20 +4,12 @@ using UnityEditor;
 
 public class ResTool : Editor
 {
-    [MenuItem("Tools/tset")]
-    public static void SearchMissResources(){
-        // 查找Assets/Resources/Prefab 所有为Prefab类型的资源的GUID
-        string[] assetGUIDs = AssetDatabase.FindAssets("t:Prefab", new string[] { "Assets/Resources/Prefab" });
-        if(assetGUIDs.Length == 0){
-            Debug.LogError("No Assets, Please check it!");
-        }
-    }
-
     [MenuItem("Tools/清理prefab中所有Missing的脚本")]
     public static void ClearAllPrefabMissingComponents()
     {
         EditorUtility.DisplayProgressBar("Modify Prefab", "Please wait...", 0);
-        string[] assetGUIDs = AssetDatabase.FindAssets("t:Prefab", new string[] { "Assets/Res/Prefab" });
+        string[] assetGUIDs = AssetDatabase.FindAssets("t:Prefab", new string[] { "Assets/Resources/Prefab" });
+        Debug.Log(assetGUIDs);
         try
         {
             for (int i = 0; i < assetGUIDs.Length; i++)
@@ -25,14 +17,6 @@ public class ResTool : Editor
                 string path = AssetDatabase.GUIDToAssetPath(assetGUIDs[i]);
                 // Debug.Log($"path:{path}");
                 GameObject pre = PrefabUtility.LoadPrefabContents(path);
-
-                DeleteRecursive(pre, (go) =>
-                {
-                    GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
-                });
-                PrefabUtility.SaveAsPrefabAssetAndConnect(pre, path, InteractionMode.AutomatedAction);
-
-                EditorUtility.DisplayProgressBar($"清理Prefab丢失组件", $"当前{pre}, {i}/{assetGUIDs.Length}", i / (float)assetGUIDs.Length);
             }
         }
         catch (Exception e)
@@ -44,19 +28,4 @@ public class ResTool : Editor
         EditorUtility.ClearProgressBar();
         AssetDatabase.Refresh();
     }
-
-    /// <summary>
-    /// 遍历所有子节点
-    /// </summary>
-    /// <param name="obj"></param>
-    /// <param name="action"></param>
-    static void DeleteRecursive(GameObject obj, Action<GameObject> action)
-    {
-        action(obj);
-
-        for (int i = 0; i < obj.transform.childCount; i++)
-        {
-            DeleteRecursive(obj.transform.GetChild(i).gameObject, action);
-        }
-    }    
 }
