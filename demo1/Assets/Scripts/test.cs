d86c56c9d9f13c04076b9559ec66ed0d5482b03f
kunavya <2136570886@qq.com>
2024-09-20 15:09:55 +0800

    first commit

---
+++ demo1/Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 1;
+        Debug.Log("tset successful");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("tset successful");
+    }
+}
